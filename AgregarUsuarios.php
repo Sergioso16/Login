0c351b32812c4a6b35dd2b19bb96b08da0a35c0f
Sergio Sanchez <aevo20377@gmail.com>
2023-03-01 12:35:32 -0600

    Funcion Agregar Usuarios

--- AgregarUsuarios.php
+++ AgregarUsuarios.php
@@ -1,31 +1,30 @@
 <?php
+include('conexion.php');
 
- include('conexion.php');
+$usu = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+$rol = $_POST["rol"];
 
-$usu 	= $_POST["txtusuario"];
-$pass 	= $_POST["txtpassword"];
-$rol 	= $_POST["rol"];
+// Verificar si el usuario ya existe
+$sql = "SELECT COUNT(*) FROM login WHERE usuario = '$usu'";
+$result = mysqli_query($conn, $sql);
+$count = mysqli_fetch_array($result)[0];
 
+if ($count > 0) {
+    echo "El usuario ya existe en la base de datos.";
+} else {
+    // Insertar el usuario
+    $sql = "INSERT INTO login (usuario, pass, rol) VALUES ('$usu', '$pass', '$rol')";
+    if (mysqli_query($conn, $sql)) {
+        echo "El usuario se agregó correctamente.";
 
-
-$queryusuario = mysqli_query($conn,"INSERT INTO usuarios VALUES '$usu' and pass = '$pass' and rol = '$rol'");
-$nr 		= mysqli_num_rows($queryusuario);  
-	
-if ($nr == 1 )  
-	{ 
-		if($rol=="Usuario")
-			{	
-				header("Location: RegistrarUsuario.php");
-			}
-		else if ($rol=="Admin")
-			{
-				header("Location: RegistrarUsuario.php");
-			}
-	}
-else
-	{
-	echo "<script> alert('Usuario, contraseña o rol incorrecto.');window.location= 'index.html' </script>";
-	}
-
-/*VaidrollTeam*/
+        if ($rol == "Usuario") {
+            header("Location: pag_user.php");
+        } else if ($rol == "Admin") {
+            header("Location: pag_admin.php");
+        }
+    } else {
+        echo "Error al agregar el usuario: " . mysqli_error($conn);
+    }
+}
 ?>
